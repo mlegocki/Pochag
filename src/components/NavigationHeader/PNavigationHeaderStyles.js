c06a98afad8e127be82caa2b4023b2b734954b79
Maxwell Legocki <mdlegocki@gmail.com>
2018-12-03 17:55:28 -0500

    renamed and updated styling for NavigationHeader

---
+++ src/components/NavigationHeader/PNavigationHeaderStyles.js
@@ -0,0 +1,25 @@
+import { ROBOTO_LIGHT, LARGE } from 'src/styles/styles';
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    
+    minHeight: 40,
+    paddingTop: 20
+  },
+  defaultIcon: {
+    fontSize: 30
+  },
+  defaultTextContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingRight: 30
+  },
+  defaultText: {
+    fontSize: LARGE 
+  }
+}
+
+export default styles;
