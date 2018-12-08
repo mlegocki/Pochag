933bf49632945d80d80fafe60cb7e1e23292c0aa
Maxwell Legocki <mdlegocki@gmail.com>
2018-12-08 16:32:39 -0500

    fixed flow, linting errors. Added loading photos

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,16 @@
+// @flow
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View } from 'react-native';
 
+// Types
+import type { NavigationType, cameraRollType } from 'src/types/types';
+
 // Components
 import PText from 'src/components/Text/PText';
 import PNavigationHeader from 'src/components/NavigationHeader/PNavigationHeader';
+import PActivityIndicator from 'src/components/ActivityIndicator/PActivityIndicator';
 
 // Utilities
 import StyleUtil from 'src/utils/StyleUtil';
@@ -12,36 +18,55 @@ import StyleUtil from 'src/utils/StyleUtil';
 // Styling
 import HomeStyles from 'src/containers/Home/HomeStyles';
 
-export class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+import { getPhotos } from 'src/store/actions/cameraRollActions';
 
-    };
+const styles = StyleUtil.getStyles(HomeStyles);
+
+type PropsType = {
+  navigation: NavigationType,
+  cameraRoll: cameraRollType,
+  getPhotos: Function
+}
+
+export class Home extends Component<PropsType> {
+  constructor(props: PropsType) {
+    super(props);
   }
-  
+
+  componentDidMount() {
+    this.props.getPhotos();
+  }
+
   render() {
-    const styles = StyleUtil.getStyles(HomeStyles);
-    const { navigation } = this.props;
+    const { navigation, cameraRoll } = this.props;
+    const { loading, error, photos } = cameraRoll;
+
     return (
       <View>
         <PNavigationHeader
           navigation={navigation}
           icon={'menu'}
         />
-        <View>
-          <PText>
-            List of Stories
-          </PText>
-        </View>
+        {loading &&
+          <PActivityIndicator />
+        }
+        {!loading &&
+          <View>
+            <PText>
+              List of Stories
+            </PText>
+          </View>
+        }
       </View>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
+  const { cameraRoll } = state;
   return {
-  }
+    cameraRoll
+  };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, { getPhotos })(Home);
